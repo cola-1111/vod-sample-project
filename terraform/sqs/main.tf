9d9ffcb9238fd000d64d1241f9d5e22664be0985
cola-1111 <colacola8929@gmail.com>
2025-05-21 00:01:58 +0900

    SQSモジュール: main.tfとvariables.tfを実装（標準キューとDLQ）

--- terraform/sqs/main.tf
+++ terraform/sqs/main.tf
@@ -1,1 +1,13 @@
 // SQSキュー作成用モジュールの雛形 
+
+resource "aws_sqs_queue" "dlq" {
+  name = var.dlq_name
+}
+
+resource "aws_sqs_queue" "main" {
+  name                      = var.queue_name
+  redrive_policy            = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
+    maxReceiveCount     = var.max_receive_count
+  })
+} 
