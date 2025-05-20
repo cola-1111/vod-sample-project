1b98ae882f453dcfb9e3af01588ff6609337e553
cola-1111 <colacola8929@gmail.com>
2025-05-21 00:17:03 +0900

    IAMモジュール: main.tf, variables.tf, outputs.tf, assume_role_policy.json, mediaconvert_policy.jsonを実装

---
+++ terraform/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "lambda_role_name" {
+  description = "Lambda用IAMロール名"
+  type        = string
+}
+
+variable "mediaconvert_policy_name" {
+  description = "MediaConvert用IAMポリシー名"
+  type        = string
+} 
