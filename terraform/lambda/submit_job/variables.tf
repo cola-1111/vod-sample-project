61569b280f62a025e955fd6a99090c4a49ed44f8
cola-1111 <colacola8929@gmail.com>
2025-05-21 00:14:34 +0900

    lambda/submit_job: main.tf, variables.tf, outputs.tfを実装

---
+++ terraform/lambda/submit_job/variables.tf
@@ -0,0 +1,30 @@
+variable "function_name" {
+  description = "SubmitJob Lambda関数名"
+  type        = string
+}
+
+variable "handler" {
+  description = "Lambdaハンドラー"
+  type        = string
+}
+
+variable "runtime" {
+  description = "Lambdaランタイム"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "Lambda実行ロールのARN"
+  type        = string
+}
+
+variable "filename" {
+  description = "デプロイ用zipファイルのパス"
+  type        = string
+}
+
+variable "environment_variables" {
+  description = "環境変数のマップ"
+  type        = map(string)
+  default     = {}
+} 
