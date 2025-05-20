9ab6e4b80724ad384f769b8eb565ace14a9d3667
cola-1111 <colacola8929@gmail.com>
2025-05-20 23:59:27 +0900

    S3モジュール: main.tfとvariables.tfを実装（入力・出力バケット）

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -1,1 +1,9 @@
 // S3バケット作成用モジュールの雛形 
+
+resource "aws_s3_bucket" "input" {
+  bucket = var.input_bucket_name
+}
+
+resource "aws_s3_bucket" "output" {
+  bucket = var.output_bucket_name
+} 
---
+++ terraform/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "input_bucket_name" {
+  description = "入力用S3バケット名"
+  type        = string
+}
+
+variable "output_bucket_name" {
+  description = "出力用S3バケット名"
+  type        = string
+} 
